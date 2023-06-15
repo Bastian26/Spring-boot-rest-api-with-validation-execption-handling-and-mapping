d6dd7fd29ba836153ede71070359807ec021cb2d
Bastian26 <b.struggl@gmx.de>
2023-06-15 19:07:41 +0200

    main - implemented Global exception handling for ResourceNotFound and EmailAlreadyExists Exception

--- src/main/java/net/javaguides/springboot/repository/UserRepository.java
+++ src/main/java/net/javaguides/springboot/repository/UserRepository.java
@@ -4,6 +4,11 @@ import net.javaguides.springboot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // CustomQuery (not the standard)
+    Optional<User> findByEmail(String email);
 }
